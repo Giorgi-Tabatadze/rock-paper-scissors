a242374d9e86af710938794849d41b6256800c30
Giorgi-Tabatadze <giorgi.taba@icloud.com>
2022-04-22 18:39:00 +0400

    Add Basic UI to the game
    Added basic UI to the game. added buttons to html insted of prompt
    for user selection. Got rid of loop function which tracked the score,
    insted score is tracked by the same function that plays the game.
    Added messages displaying to the html.

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+We are going to be building a rock paper scissors game as an assignment to 
+the odin project. In order to build rock paper scissors game we are going to
+be using basic knowladge aquired during our javascript course. We are going
+to be using function and logic operators in order to get this done, as well
+as understanding how to start a project from the scratch and plan out program
+development and demployment. 
 # rock-paper-scissors
--- javascript.js
+++ javascript.js
@@ -22,129 +22,102 @@ function generateRandom(min, max) {
     return rand;
 }
 
-//create function "computerPlay" with no parameters required
+/*function that generates random computer selection using numbers, converted
+into string and returns it*/
 function computerPlay() {
-//declare variable with name "n"
-//n is equal to random number between 1 and 3. 
   let n = generateRandom(1, 4);
-//if n is equal to 1 return a string - "rock"
   if (n === 1) { 
     return "ROCK";
-//else if n is equal to 2 return a string - "paper"
   } else if (n === 2) { 
     return "PAPER";
-//else return a string- "scissors"
   } else 
     return "SCISSORS";
 }
 
-
-//create function playRound - parameters(computerSelection, playerSelection)
-function playRound() {
-//computerSelection is equal to result of computerPlay function. 
+/* Play a round using user input and increment winner score to track who wins. 
+if one of the users gets 5 they win and game start over.*/
+function playRound(playerSelection) {
+  playerSelection = playerSelection.target.id;
   let computerSelection = computerPlay();
-//playerSelection is equal to lower cased user input that is taken using a prompt.
-  let playerSelection = prompt("Rock, Paper or Scissors?").toUpperCase();
-//if computerSelection is equal to playerSelection return a string - "its a tie!"
   if (computerSelection === playerSelection) 
   {
-    return "its a tie!";
+    message = ("its a tie!")
   }
-//if computerSelection is rock and playerSelection is scissors return a string - 
-//"You Lose! Rock beats Scissors"
   else if (computerSelection === "ROCK" 
             && playerSelection === "SCISSORS") 
   {
-    return "You Lose! Rock beats Scissors";
+    message = ("You Lose! Rock beats Scissors")
+    computerScore++;
   }
-//if computerSelection is paper and playerSelection is rock return a string - 
-//"You Lose! Paper beats Rock"
   else if (computerSelection === "PAPER" 
             && playerSelection === "ROCK")
   {
-    return "You Lose! Paper beats Rock";
+    message = ("You Lose! Paper beats Rock")
+    computerScore++;
   }
-//if computerSelection is scissors and playerSelection is paper return a string -
-//"You Lose! Scissors beat paper"
+
   else if (computerSelection === "SCISSORS"
           && playerSelection === "PAPER")
   {
-    return "You Lose! Scissors beat paper";
+    message = ("You Lose! Scissors beat paper")
+    computerScore++;
   }
-//if computerSelection is rock and playerSelection is paper return a string -
-//"You Win! Paper beats Rock"
   else if (computerSelection === "ROCK"
           && playerSelection === "PAPER")
   {
-    return "You Win! Paper beats Rock";
+    message = ("You Win! Paper beats Rock")
+    playerScore++;
   }
-//if computerSelection is paper and playerSelection is scissors return a string -
-//"You Win! Scissors beat Rock"
   else if (computerSelection === "PAPER"
           && playerSelection === "SCISSORS")
   {
-    return "You Win! Scissors beat Rock";
+    message = ("You Win! Scissors beat Rock");
+    playerScore++;
   }
-//if computerSelection is scissors and playerSelection is rock return a string- 
-//"You Win! Rock beats Scisssors"
   else if (computerSelection === "SCISSORS"
           && playerSelection === "ROCK")
   {
-    return "You Win! Rock beats Scisssors";
+    message = ("You Win! Rock beats Scisssors");
+    playerScore++;
   }
-
-//else return a string - "Incorrect input! Please try again"
   else 
   {
-    return "Incorrect input! Please try again";
+    message = ("Incorrect input! Please try again");
   }
+     score = (`Your score is: ${playerScore} and Computer score is: ${computerScore}`)
+  if (computerScore === 5) 
+  {
+    message = ("Loser! You have lost the game!, press any key to start again")
+    computerScore = 0;
+    playerScore = 0;
+  }
+  if (playerScore === 5)
+  {
+    message = "Congratulations! You have won the game! press any key to start again"
+    computerScore = 0;
+    playerScore = 0;
+  }
+  mainScreen.innerText = `${score}
+                          ${message}`
 }
 
-//create function game with no parameters
-function game() {
-//declare variable computerScore and set it to 0 
-  let computerScore = 0;
-//declare variable playerScore and set it to 0 
-  let playerScore = 0;
-//declare variable message and set it to empty string
-  let message = ""
-//create a for loop that is going to run until computer or player get 5 wins.
-  for (let i = 0; i < 10; i++) {
-//message is equal to playRound function result 
-    message = playRound();
-//if message contains "You Lose" 
-//increase computerScore by one
-    if (message.includes("You Lose"))
-    {
-      computerScore++;
-    }
-//else if message contains "You Win"
-//increase playerScore by one
-    else if (message.includes("You Win")) 
-    {
-      playerScore++
-    }
 
-//else decrease for loop variable by one
-    else
-    {
-      i--
-    }
-//console log message
-//console log "Your score is: playerScore  Computer score is: computerScore"
-    console.log(message);
-    console.log(`Your score is: ${playerScore} and Computer score is: ${computerScore}`)
+let computerScore = 0;
+let playerScore = 0;
 
-//if computerScore is equal to 5 console log "Loser! You have lost the game!"
-    if (computerScore === 5)
-    {
-      return console.log("Loser! You have lost the game!")
-    }
-//if playerScore is equal to 5 console log "Congratulations! You have won the game!"
-    if (playerScore === 5)
-    {
-      return console.log("Congratulations! You have won the game!")
-    }
-  }
-}
+const mainScreen = document.querySelector("#message")
+const buttons = Array.from(document.querySelectorAll("button"));
+console.log(buttons)
+buttons.forEach(button => {button.addEventListener("click", playRound)});
+
+
+
+/*function that plays rounds until one of the users gets 5 wins*/
+
+
+/*add event listeners to all the buttons in div. 
+function that plays 1 round using playround function where I pass selection. 
+track score. 
+if one reached 5 restart game. 
+*/
 
